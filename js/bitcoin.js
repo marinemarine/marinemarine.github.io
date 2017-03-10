38861dfc3907ad576c5647b4ba5d4c867a1c2265
marinemarine <mezin.marine@gmail.com>
2017-03-10 14:22:57 +0100

    Update bitcoin.js

--- js/bitcoin.js
+++ js/bitcoin.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 		async : false,
 
 		success : function(data) {
-			$('#bitcoin_block_number').append(data.height);
+			$('#bitcoin_block_number').append(data.blocks);
 		},
 
 		error : function(xhr, status, err) {
